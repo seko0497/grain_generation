ab81abd3b49e46dd141a9ee43c3bc9efd9f4313e
kons <sebastian.kons@tu-dortmund.de>
2023-02-17 12:39:35 +0100

    load target masks

--- dataset_wear.py
+++ dataset_wear.py
@@ -1,4 +1,7 @@
-from torch.utils.data import Dataset
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import os
@@ -18,10 +21,21 @@ class WearDataset(Dataset):
         self.inputs = [f"{self.root_dir}/features/1/{filename}.png"
                        for filename in self.files]
 
+        self.targets = [f"{self.root_dir}/target/1/{filename}.npy"
+                        for filename in self.files]
+
         self.transforms = transforms.Compose([
             transforms.ToTensor(),
             transforms.CenterCrop(raw_img_size[0]),
             transforms.Resize(img_size),
+            transforms.Lambda(lambda x: x * 2 - 1)
+        ])
+
+        self.target_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.CenterCrop(raw_img_size[0]),
+            transforms.Resize(
+                img_size, interpolation=transforms.InterpolationMode.NEAREST),
             transforms.Lambda(lambda x: x * 2 - 1)
         ])
 
@@ -33,17 +47,24 @@ class WearDataset(Dataset):
 
         inp = Image.open(f"{self.inputs[index]}")
         inp = self.transforms(inp)
+        # print(np.unique(inp))
+
+        trg = np.load(self.targets[index])
+        trg = trg * 255 / 2
+        trg = trg[:, :, None].astype(np.uint8())
+
+        trg = self.target_transforms(trg)
 
         return {"I": inp}
 
 
 # DEBUG
-# wear_dataset = WearDataset(
-#     "data/RT100U_processed/train", (448, 576), (128, 128))
-# wear_dataloader = DataLoader(wear_dataset, batch_size=4)
-# for batch in wear_dataloader:
-#     # pass
-#     image = torch.moveaxis(batch["I"][0], 0, -1).numpy()
-#     image = (image + 1) / 2
-#     plt.imshow(image)
-#     plt.show()
+wear_dataset = WearDataset(
+    "data/RT100U_processed/train", (448, 576), (128, 128))
+wear_dataloader = DataLoader(wear_dataset, batch_size=4)
+for batch in wear_dataloader:
+    pass
+    # image = torch.moveaxis(batch["I"][0], 0, -1).numpy()
+    # image = (image + 1) / 2
+    # plt.imshow(image)
+    # plt.show()
--- generate_samples.py
+++ generate_samples.py
@@ -1,9 +1,12 @@
 import os
 from matplotlib import pyplot as plt
 import torch
+from tqdm import tqdm
 from diffusion import Diffusion, get_schedule
 from unet import Unet
 from PIL import Image
+from torch.utils.data import DataLoader
+from dataset_wear import WearDataset
 
 checkpoint = torch.load("wear_generation/best.pth")
 wandb_name = "aflame-candles-301"
@@ -14,11 +17,11 @@ model_dim = 128
 dim_mults = (1, 2, 4, 8)
 num_resnet_blocks = 2
 
-beta_0 = 0.000025
-beta_t = 0.005
-timesteps = 4000
+beta_0 = 0.00005
+beta_t = 0.01
+timesteps = 2000
 schedule = "cosine"
-sampling_steps = 100
+sampling_steps = 200
 
 loss = "hybrid"
 
@@ -44,34 +47,72 @@ diffusion = Diffusion(
     device,
     use_wandb=False)
 
-samples = diffusion.sample(model, 9, checkpoint["epoch"], sampling_steps)
+samples = diffusion.sample(model, 1, checkpoint["epoch"], sampling_steps)
 
 save_folder = f"wear_generation/samples/{wandb_name}"
 
-if grid:
+data_loader = DataLoader(WearDataset(
+    f"data/RT100U_processed/train",
+    raw_img_size=(448, 576),
+    img_size=(64, 64)
+), batch_size=1,
+    shuffle=False)
 
-    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
-    for ax in axs.flatten():
-        ax.axis('off')
+sims = []
 
-    image_idx = 0
-    for i in range(3):
-        for j in range(3):
-            print(image_idx)
-            sample = (samples[image_idx] * 255).type(torch.uint8)
-            sample = torch.moveaxis(sample, 0, -1).cpu().detach().numpy()
-            axs[i, j].imshow(sample)
-            image_idx += 1
+for batch in tqdm(data_loader):
 
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig(f'{save_folder}/image_grid.png')
+    train_image = batch["I"]
+    train_image = (train_image + 1) / 2
 
-else:
+    sims.append(torch.nn.CosineSimilarity()(
+        torch.flatten(train_image, start_dim=1),
+        torch.flatten(samples.cpu(), start_dim=1)))
 
-    for i, sample in samples:
-        sample = (sample * 255).type(torch.uint8)
-        sample = torch.moveaxis(sample, 0, -1).cpu().detach().numpy()
-        image = Image.fromarray(sample)
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
-        image.save(f"{save_folder}/sample_{i}.png")
+print(torch.argmax(torch.stack(sims)))
+print(torch.max(torch.stack(sims)))
+
+match = list(data_loader)[torch.argmax(torch.stack(sims))]["I"]
+
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
+for ax in axs.flatten():
+    ax.axis('off')
+
+match = (match + 1) / 2
+match = (match[0] * 255).type(torch.uint8)
+match = torch.moveaxis(match, 0, -1).cpu().detach().numpy()
+
+sample = (samples[0] * 255).type(torch.uint8)
+sample = torch.moveaxis(sample, 0, -1).cpu().detach().numpy()
+
+axs[0].imshow(match)
+axs[1].imshow(sample)
+plt.show()
+
+# if grid:
+
+#     fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
+#     for ax in axs.flatten():
+#         ax.axis('off')
+
+#     image_idx = 0
+#     for i in range(3):
+#         for j in range(3):
+#             print(image_idx)
+#             sample = (samples[image_idx] * 255).type(torch.uint8)
+#             sample = torch.moveaxis(sample, 0, -1).cpu().detach().numpy()
+#             axs[i, j].imshow(sample)
+#             image_idx += 1
+
+#     plt.subplots_adjust(wspace=0, hspace=0)
+#     plt.savefig(f'{save_folder}/image_grid.png')
+
+# else:
+
+#     for i, sample in samples:
+#         sample = (sample * 255).type(torch.uint8)
+#         sample = torch.moveaxis(sample, 0, -1).cpu().detach().numpy()
+#         image = Image.fromarray(sample)
+#         if not os.path.exists(save_folder):
+#             os.makedirs(save_folder)
+#         image.save(f"{save_folder}/sample_{i}.png")
